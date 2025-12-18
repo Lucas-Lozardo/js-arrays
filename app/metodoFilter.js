99d9b921ae1545ce968dd4e425c23d8a8406d2df
Lucas Lozardo <lucas_losardo@hotmail.com>
2025-12-17 22:15:53 -0300

    Implementado metodoFilter.js para filtrar por categoria

---
+++ app/metodoFilter.js
@@ -0,0 +1,8 @@
+const botoes = document.querySelectorAll('.btn');
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros));
+
+function filtrarLivros(){
+    const elementoBtn = document.getElementById(this.id);
+    let livrosFiltrados = livros.filter(livro => livro.categoria == elementoBtn.value);
+    exibirOsLivrosNaTela(livrosFiltrados);
+}
